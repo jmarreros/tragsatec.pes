8b5db1a87f5a50a625fe85523955b3bf05821a49
Jhon Marreros Guzmán <j_marreros@hotmail.com>
2025-04-24 09:32:18 -0500

    first commit

---
+++ src/main/java/tragsatec/pes/PesApplication.java
@@ -0,0 +1,13 @@
+package tragsatec.pes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PesApplication.class, args);
+	}
+
+}
