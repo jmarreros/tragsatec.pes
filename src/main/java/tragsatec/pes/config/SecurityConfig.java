9a86db79ca67811f4dd60032fc71c7d686445cc6
Jhon Marreros Guzmán <j_marreros@hotmail.com>
2025-05-10 11:41:36 -0500

    Corrección CORS

--- src/main/java/tragsatec/pes/config/SecurityConfig.java
+++ src/main/java/tragsatec/pes/config/SecurityConfig.java
@@ -13,6 +13,8 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
+import static org.springframework.security.config.Customizer.withDefaults;
+
 @Configuration
 public class SecurityConfig {
 
@@ -29,6 +31,7 @@ public class SecurityConfig {
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http
+                .cors(withDefaults())
                 .csrf(AbstractHttpConfigurer::disable)
                 .authorizeHttpRequests((authorize) -> authorize
                         .requestMatchers("/login").permitAll()
